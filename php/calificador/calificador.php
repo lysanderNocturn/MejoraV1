32d762d4a2d580e1ba2bf519fa794b2cdd352ed9
lysanderNocturn <posadanomuertos@gmail.com>
2024-07-15 13:43:02 -0600

    150724-2

---
+++ php/calificador/calificador.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de actividades</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/globals.css">
+    <link rel="stylesheet" href="../../css/ventanilla.css">
+    
+    <style>
+       
+
+        .card {
+            border: none;
+            border-radius: 15px;
+            box-shadow: 0 10px 20px rgba(100, 205, 255, 1);
+            background-color: rgba(255, 255, 255, 0.4);
+            margin: auto;
+            margin-top: 30px;
+            padding: 15px;
+            transition: all 0.3s ease-in-out;
+        }
+
+        .card:hover {
+            transform: scale(1.0005);
+        }
+
+        .form-group label {
+            font-weight: bold;
+        }
+
+        .btn-primary {
+            background-color: #0d47a1;
+            border-color: #0d47a1;
+            transition: all 0.3s ease-in-out;
+        }
+
+        .btn-primary:hover {
+            background-color: #2962ff;
+            border-color: #2962ff;
+        }
+    </style>
+</head>
+<body>
+    <!-- nav bar -->
+    <?php include ('nav.php')?>
+
+</body>
+</html>
---
+++ php/calificador/nav.php
@@ -0,0 +1,82 @@
+<?php
+// Iniciar la sesión
+session_start();
+
+$username = $_SESSION["username"];
+
+
+
+// Verificar si la variable de sesión 'username' está establecida
+if (!isset($_SESSION["username"])) {
+    // No hay una sesión iniciada, redirigir al usuario a la página de inicio
+    header("Location: ../../index.php");
+    exit(); // Finalizar el script para evitar que se ejecute más código
+}
+?>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container">
+    <a class="navbar-brand" href="ventanilla.php">Planeación</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ml-auto"> <!-- Agregamos la clase ml-auto aquí -->
+        <li class="nav-item">
+          <a class="nav-link" href="ventanilla.php">Realizar trámite</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="registros.php">Registros realizados</a>
+    
+        <li class="nav-item">
+          <a class="nav-link" href="extra.php">Añadir datos</a>
+        </li>
+        </li>
+          <a class="nav-link" href="PerfilV.php">usuario: <?php echo $username; ?></a>
+        </li>
+      </ul>
+      <ul class="navbar-nav"> <!-- Lista separada para "Cerrar sesión" -->
+      
+        <li class="nav-item">
+          <a class="nav-link" href="../cerrar_sesion.php">Cerrar sesión</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+<style>
+  /* Estilos personalizados para la barra de navegación */
+.navbar-brand {
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: #f0f8ff; /* Color blanco */
+}
+
+.navbar-toggler {
+  border-color: #f0f8ff; /* Color blanco */
+} 
+
+.navbar-toggler-icon {
+  background-color: #f018ff; /* Color blanco */
+}
+
+.navbar-nav .nav-link {
+  font-size: 1.1rem;
+  color: #f0f8ff; /* Color blanco */
+}
+
+.navbar-nav .nav-link:hover {
+  background-color: rgba(185, 25, 255, 0.3); /* Color de fondo ligeramente transparente al pasar el cursor */
+}
+
+</style>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
